b18e9b629527de772267e8fa6f5f8aeb66029338
friend1ws <friend1ws@gmail.com>
2015-09-24 14:06:55 +0900

    completion until filtering bam

--- lib/genomon_expression/run.py
+++ lib/genomon_expression/run.py
@@ -9,14 +9,18 @@ def main(args):
     input_bam = args.bam_file
     output = args.output
     bedtools_path = args.bedtools_path
+    mapq_thres = args.q
 
-    if not os.path.dirname(output):
-       os.makedirs(os.path.dirname(output))
+    output_dir = os.path.dirname(output)
+    if not os.path.exists(output_dir):
+       os.makedirs(output_dir)
  
     utils.filterImproper(input_bam, output + ".filt.bam", mapq_thres)
 
+    """
     hOUT = open(output + ".filt.bed12", 'w')
     subprocess.call([bedtools_path + "/bedtools", "bamtobed", "-bed12", "-i", output + ".filt.bam"], stdout = hOUT)
     hOUT.close()
+    """
 
 
