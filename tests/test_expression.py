82051c3ea1b4d8018380f3e7ead9642c24b919cb
friend1ws <friend1ws@gmail.com>
2019-09-24 07:11:03 +0000

    add test of python3.7

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -20,11 +20,11 @@ class TestMain(unittest.TestCase):
         answer_file = cur_dir + "/data/expression/MCF-7_22.sym2fpkm.txt"
         output_prefix = output_file.replace(".sym2fpkm.txt", '')
 
+        print(output_file)
         args = self.parser.parse_args([input_bam, output_prefix, "--grc"])
         genomon_expression.run.expression_main(args)
 
         self.assertTrue(filecmp.cmp(output_file, answer_file, shallow=False))
-
         shutil.rmtree(tmp_dir)
 
 if __name__ == "__main__":
